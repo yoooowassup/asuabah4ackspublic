5f218c41792c9c0e815023829bf85c260166e96e
Venus Ring <Fmeat51@gmail.com>
2026-01-05 20:34:11 -0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,5 +1,10 @@
+local PlaceId = 1458767429;
+
+if game.PlaceId ~= PlaceId then return; end;
+
 pcall(function()
 queue_on_teleport([[
+  if game.PlaceId ~= PlaceId then return; end;
   loadstring(game:HttpGet("https://raw.githubusercontent.com/yoooowassup/asuabah4ackspublic/refs/heads/main/main.lua"))()
 ]])
 end)
