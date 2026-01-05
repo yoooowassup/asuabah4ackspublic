523677a0a43b9fe2ef5e7c898baa41decfa44b49
yoooowassup <165237304+yoooowassup@users.noreply.github.com>
2026-01-05 16:44:17 -0600

    Add loader.lua to execute remote script

---
+++ loader.lua
@@ -0,0 +1,7 @@
+pcall(function()
+queue_on_teleport([[
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/yoooowassup/asuabah4ackspublic/refs/heads/main/main.lua"))()
+]])
+end)
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/yoooowassup/asuabah4ackspublic/refs/heads/main/main.lua"))()
